381df01da5bcca6f3fa7c80ac827a898d06ee959
Belchior Oliveira <belchiorso@gmail.com>
2014-12-15 20:31:25 -0200

    reimplementation of Macwidget is complete

--- js/calc-macwidget.js
+++ js/calc-macwidget.js
@@ -29,35 +29,10 @@ Macwidget.prototype.skin = function (selector) {
   _this.equality = _this.html.querySelector('.btn[data-name=equality]');
   _this.dot = _this.html.querySelector('.btn[data-name=dot]');
   _this.clear = _this.html.querySelector('.btn[data-name=clear]');
-  _this.madd = _this.html.querySelector('.btn[data-name=madd]');
-  _this.msubtract = _this.html.querySelector('.btn[data-name=msubtract]');
-  _this.mclear = _this.html.querySelector('.btn[data-name=mclear]');
-  _this.mrecall = _this.html.querySelector('.btn[data-name=mrecall]');
-
-  _this.memory = (function () {
-    var operation = '';
-    var operand = 0;
-    var get = function (op) {
-      if (op === 'operation') {
-        return operation;
-      } else if (op === 'operand') {
-        return operand;
-      }
-      return;
-    };
-    var set = function () {
-      if (arguments[0] === 'operation') {
-        operation = arguments[1] || '';
-      } else if (arguments[0] === 'operand') {
-        operand = arguments[1] || 0;
-      }
-      return _this;
-    };
-    return {
-      get: get,
-      set: set
-    };
-  })();
+  _this.memoryAdd = _this.html.querySelector('.btn[data-name=madd]');
+  _this.memorySubtract = _this.html.querySelector('.btn[data-name=msubtract]');
+  _this.memoryClear = _this.html.querySelector('.btn[data-name=mclear]');
+  _this.memoryRecall = _this.html.querySelector('.btn[data-name=mrecall]');
 
   _this.display = (function () {
     var content = _this.html.querySelector('.display');
@@ -92,6 +67,64 @@ Macwidget.prototype.skin = function (selector) {
     };
   })();
 
+  _this.recorder = (function () {
+    var operation = '';
+    var operand = 0;
+    var get = function (op) {
+      if (op === 'operation') {
+        return operation;
+      } else if (op === 'operand') {
+        return operand;
+      }
+      return;
+    };
+    var set = function () {
+      if (arguments[0] === 'operation') {
+        operation = arguments[1] || '';
+      } else if (arguments[0] === 'operand') {
+        operand = arguments[1] || 0;
+      }
+      return _this;
+    };
+    return {
+      get: get,
+      set: set
+    };
+  })();
+
+  _this.memory = (function () {
+    var slot = 0;
+
+    var add = function () {
+      slot = _this.sum(slot, _this.display.get());
+      _this.inactiveOperators();
+      _this.equalityWasClicked = true;
+    };
+
+    var subtract = function () {
+      slot = _this.subtract(slot, _this.display.get());
+      _this.inactiveOperators();
+      _this.equalityWasClicked = true;
+    };
+
+    var clear = function () {
+      slot = 0;
+    };
+
+    var recall = function () {
+      _this.display.set(slot);
+      _this.inactiveOperators();
+      _this.equalityWasClicked = true;
+    };
+
+    return {
+      add: add,
+      subtract: subtract,
+      clear: clear,
+      recall: recall,
+    };
+  })();
+
   _this.showError = function () {
     _this.html.classList.add('calcError');
     setTimeout(function () {
@@ -122,7 +155,7 @@ Macwidget.prototype.skin = function (selector) {
 
   _this.calculateEvent = function () {
     var result;
-    var operation = _this.memory.get('operation');
+    var operation = _this.recorder.get('operation');
     var operand = _this.display.get();
 
     if (!operation || !operand) {
@@ -130,11 +163,11 @@ Macwidget.prototype.skin = function (selector) {
     }
 
     if (_this.equalityWasClicked) {
-      result = _this[operation](operand, _this.memory.get('operand'));
+      result = _this[operation](operand, _this.recorder.get('operand'));
 
     } else {
-      result = _this[operation](_this.memory.get('operand'), operand);
-      _this.memory.set('operand', operand);
+      result = _this[operation](_this.recorder.get('operand'), operand);
+      _this.recorder.set('operand', operand);
     }
 
     _this.display.set(result);
@@ -152,15 +185,15 @@ Macwidget.prototype.skin = function (selector) {
       return _this.showError();
     }
 
-    if (_this.memory.get('operation') && _this.memory.get('operand') && _this.equalityWasClicked === false) {
+    if (_this.recorder.get('operation') && _this.recorder.get('operand') && _this.equalityWasClicked === false) {
       _this.calculateEvent()
-      .memory.set('operand', _this.display.get());
+      .recorder.set('operand', _this.display.get());
 
     } else {
-      _this.memory.set('operand', operand);
+      _this.recorder.set('operand', operand);
     }
 
-    _this.memory.set('operation', this.getAttribute('data-action'));
+    _this.recorder.set('operation', this.getAttribute('data-action'));
     _this.activeOperation(this);
     _this.equalityWasClicked = false;
   };
@@ -172,35 +205,13 @@ Macwidget.prototype.skin = function (selector) {
     }
     return _this.showError();
   };
+
   _this.clearEvent = function () {
     _this.display.set('');
-    _this.memory.set('operation', '');
-    _this.memory.set('operand', 0);
+    _this.recorder.set('operation', '');
+    _this.recorder.set('operand', 0);
     _this.inactiveOperators();
   };
-  // _this.maddEvent = function () {
-  //   _this.memory = Macwidget.prototype.sum(
-  //     _this.memory,
-  //     Macwidget.prototype.format(_this.display.get())
-  //   );
-  //   console.log(_this.memory);
-  // };
-  // _this.msubtractEvent = function () {
-  //   _this.memory = Macwidget.prototype.subtract(
-  //     _this.memory,
-  //     Macwidget.prototype.format(_this.display.get())
-  //   );
-  //   console.log(_this.memory);
-  // };
-  // _this.mclearEvent = function () {
-  //   _this.memory = 0;
-  //   console.log(_this.memory);
-  // };
-  // _this.mrecallEvent = function () {
-  //   _this.display.set(_this.memory);
-  //   _this.display.output.set('');
-  //   console.log(_this.memory);
-  // };
 
   _this.number0.addEventListener('click', _this.numbersEvent);
   _this.number1.addEventListener('click', _this.numbersEvent);
@@ -219,10 +230,9 @@ Macwidget.prototype.skin = function (selector) {
   _this.equality.addEventListener('click', _this.calculateEvent);
   _this.dot.addEventListener('click', _this.dotEvent);
   _this.clear.addEventListener('click', _this.clearEvent);
-  // _this.madd.addEventListener('click', _this.maddEvent);
-  // _this.msubtract.addEventListener('click', _this.msubtractEvent);
-  // _this.mclear.addEventListener('click', _this.mclearEvent);
-  // _this.mrecall.addEventListener('click', _this.mrecallEvent);
+  _this.memoryAdd.addEventListener('click', _this.memory.add);
+  _this.memorySubtract.addEventListener('click', _this.memory.subtract);
+  _this.memoryClear.addEventListener('click', _this.memory.clear);
+  _this.memoryRecall.addEventListener('click', _this.memory.recall);
 
-  // return _this;
 };
