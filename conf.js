d4a7deffbeafe2aa355eea961f88f27de0403191
Belchior Oliveira <belchiorso@gmail.com>
2014-12-19 09:20:43 -0200

    bug fix at firefox

--- conf.js
+++ conf.js
@@ -1,17 +1,13 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  // specs: ['test/protractor_*'],
-  specs: ['test/protractor_test_macwidget.js'],
+  specs: ['test/protractor_*.js'],
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
     isVerbose: true
   },
   multiCapabilities: [
-    // {browserName: 'firefox'},
-    {
-      browserName: 'chrome',
-      chromeOptions: {args: ['--test-type']}
-    }
+    {browserName: 'firefox'},
+    {browserName: 'chrome', chromeOptions: {args: ['--test-type']}}
   ]
 };
