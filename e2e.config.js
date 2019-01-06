6cf6afebb9d061d0bf4cad7b1a18953db08026ef
Belchior Oliveira <belchior@outlook.com>
2019-01-06 17:37:11 -0200

    added e2e test provided be puppeteer

---
+++ e2e.config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  appUrl: 'http://localhost:3000',
+  launchOptions: {
+    headless: true,
+    devtools: false,
+    defaultViewport: {
+      width: 0,
+      height: 0,
+    },
+  },
+};
