8596bd2c7cc202f5d970778bf0741fa5dc331df7
Belchior Oliveira <belchior@outlook.com>
2019-01-18 00:17:44 -0200

    added cypress to project

--- config/jest.config.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-  preset: "jest-puppeteer",
-  // setupTestFrameworkScriptFile: "expect-puppeteer",
-  rootDir: '../',
-  bail: true,
-  testMatch: ['<rootDir>/src/**/*.e2e.js'],
-  // testMatch: ['<rootDir>/src/components/Calc/Terminal/**/*.e2e.js'],
-};
--- config/postbuild.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p build/calc
-ls build/ |\
-grep -v 'calc' |\
-xargs -i@ cp -r "$(pwd)/build/@" 'build/calc/'
