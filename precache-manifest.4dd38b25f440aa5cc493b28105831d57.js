f2894c8d07384d0a3f1e9f821f5c65d030eabe0f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-15 23:26:32 +0000

    Deploy belchior/calc to github.com/belchior/calc.git:gh-pages

---
+++ precache-manifest.4dd38b25f440aa5cc493b28105831d57.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "774765496913dad2cd5978a29a8eda66",
+    "url": "/calc/index.html"
+  },
+  {
+    "revision": "1baeebc4741ff0853543",
+    "url": "/calc/static/css/main.99a38b28.chunk.css"
+  },
+  {
+    "revision": "752714255f18ca96bc79",
+    "url": "/calc/static/js/2.e9d5445e.chunk.js"
+  },
+  {
+    "revision": "1baeebc4741ff0853543",
+    "url": "/calc/static/js/main.9e672434.chunk.js"
+  },
+  {
+    "revision": "600174be44eb6ab33f25",
+    "url": "/calc/static/js/runtime~main.8ebb2047.js"
+  },
+  {
+    "revision": "b8d1a0910468085af6c807892245f525",
+    "url": "/calc/static/media/MontserratAlternates-Regular.b8d1a091.ttf"
+  },
+  {
+    "revision": "df382598096350122ce7e386eba09439",
+    "url": "/calc/static/media/icon-custom.df382598.svg"
+  },
+  {
+    "revision": "79531950e6aa22fee73275b9dc03cd0d",
+    "url": "/calc/static/media/icon-github.79531950.svg"
+  },
+  {
+    "revision": "5cbd6b21bf47513e6398b0ca192dd878",
+    "url": "/calc/static/media/icon-macwidget.5cbd6b21.svg"
+  },
+  {
+    "revision": "0da3305d03ee09613995277219744885",
+    "url": "/calc/static/media/icon-terminal.0da3305d.svg"
+  }
+]);
